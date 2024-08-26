4f808f83f105630a48e46254817e285658a8565c
Mobassir Mahmud Dibbo <115061123+dibbo1718@users.noreply.github.com>
2024-08-26 14:20:03 +0600

    Add files via upload

---
+++ pb 3.py
@@ -0,0 +1,6 @@
+age = 45
+salary = 1456.8
+name = "John"
+print(age)
+print(salary)
+print(name)
---
+++ pb 5.py
@@ -0,0 +1,12 @@
+a = 11
+b = 7
+add = a + b
+sub = a - b
+mul = a * b
+mod = a % b
+p = a ** b
+print(add)
+print(sub)
+print(mul)
+print(mod)
+print(p)
